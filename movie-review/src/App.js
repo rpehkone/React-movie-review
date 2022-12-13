b0448a9c7274c279cfa4b1e07159e0b4df2b842f
rpehkone <pehkonenroni@gmail.com>
2022-12-13 21:06:16 +0200

    add Page selection buttons

--- movie-review/src/App.js
+++ movie-review/src/App.js
@@ -2,6 +2,39 @@ import React from 'react';
 import {MovieQuery} from './MovieQuery.js';
 import './App.css';
 
+class PageButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.asdf = 0;
+	}
+	//onclick index
+	render() {
+		var name = this.props.name;
+		return (
+			<p>{name}</p>
+			// <button type='button' key={value} onClick="this.click('{%index}');">{value}</button>
+			);
+	}
+}
+
+class PageSelector extends React.Component {
+	click(i) {
+		console.log("page = " + String(i));
+	}
+	page = 0;
+	pageNames = ["Activity", "Profile", "Movies", "Search"];
+	pages_container = this.pageNames.map((value, get_index) => {
+		return <PageButton key={value} name={value} index={get_index}></PageButton>
+	});
+	render() {
+		return (
+			<div>
+				{this.pages_container}
+			</div>
+		);
+	}
+}
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -14,6 +47,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<PageSelector/>
 				<MovieQuery/>
 				<button type='button' onClick={this.click}>Click me</button>
 			</div>
--- movie-review/src/MovieQuery.js
+++ movie-review/src/MovieQuery.js
@@ -4,12 +4,24 @@ import {MovieThumbnail} from './MovieThumbnail.js';
 export class MovieQuery extends React.Component {
 	movies = ["harry potter", "home alone", "blade runner", "coco", "spider-man"];
 	movie_container = this.movies.map((value, index) => {
-		return <MovieThumbnail name={value} />
+		return <MovieThumbnail key={value} name={value} />
 	});
+	SetDecade(decade) {
+		this.decade = decade;
+		console.log(this.decade);
+	}
 	render() {
 		return (
 			<div className='movie-thumbnail-container'>
-				<h1>Hello</h1>
+				<h5>Movies</h5>
+				<select key={0} name="decade" id="decade-selector" onChange={(decade) => this.SetDecade(decade.value)} >
+					<option key={0} decade={2020}>2020s</option>
+					<option key={1} decade={2010}>2010s</option>
+					<option key={2} decade={2000}>2000s</option>
+					<option key={3} decade={1990}>1990s</option>
+					<option key={4} decade={1980}>1980s</option>
+					<option key={5} decade={1970}>1970s</option>
+				</select>
 				{this.movie_container}
 			</div>
 		);
